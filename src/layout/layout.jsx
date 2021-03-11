78b8a629f90ecf760ae51483f77cbe40172d4d6e
Binny V A <Binny V A>
2021-03-12 00:55:35 +0530

    Updated all libraries to sensible versions

---
+++ src/layout/layout.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./header"
+
+export default function Layout({ children, title }) {
+  return (
+    <div id="layout">
+      <Header title={ title } />
+      { children }
+    </div>
+  )
+}
