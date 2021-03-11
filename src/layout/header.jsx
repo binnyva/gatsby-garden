edf5d9842430299c15a3fd87374c58915137030f
Binny V A <Binny V A>
2021-03-12 01:05:28 +0530

    Added helmet library - and with it Head tags to the project

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery,graphql,Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 export default function Header({ title }) {
   const data = useStaticQuery(
@@ -15,6 +16,12 @@ export default function Header({ title }) {
   )
 
   return (
+    <>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>{data.site.siteMetadata.title }</title>
+        </Helmet>
       <h1><Link to={`/`}>{data.site.siteMetadata.title } { title ? ` : ${title}` : null }</Link></h1>
+    </>
   )
 }
