30824e805d023498ffa5850b469ddc7f20a7cfce
Binny V A <binnyva@gmail.com>
2022-04-17 14:36:52 +0530

    Added the purgecss config

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,11 +127,10 @@ module.exports = {
         // printRejected: true, // Print removed selectors and processed file names. Use for debugging.
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['tippy.js/', 'tooltip.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
-          // https://purgecss.com/configuration.html#options
-          // safelist: ['safelist'], // Don't remove this selector
+          safelist: ['.tippy-box'], // Don't remove this selector
         },
         // More options defined here https://purgecss.com/configuration.html#options
       },
